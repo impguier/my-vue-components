1cce057b00e75bbdaa9241d5d02db5a492d66ded
gengchenlong <gengchenlong@wyjq.com>
2021-09-13 17:43:57 +0800

    go

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
+export default {
+    input: './src/index.js',
+    output: [
+        {
+            file: './dist/easy-beautiful-umd.js',
+            format: 'umd',
+            name: 'easyBeautiful'
+        },{
+            file: './dist/easy-beautiful-es.js',
+            format: 'es',
+            name: 'easyBeautiful'
+        },{
+            file: './dist/easy-beautiful-cjs.js',
+            format: 'cjs',
+            name: 'easyBeautiful'
+        }
+
+    ],
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        commonjs(),
+        postcss({
+            plugins: [
+                autoprefixer(),
+                // cssnano()
+            ]
+        })
+    ]
+}
